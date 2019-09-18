f393a877a55e66909bfcbb4f6d02bdd84e304e5a
chenjuny <cjy_java@126.com>
2019-09-18 16:44:42 +0800

    wordcount 解析

--- sparkstudy01/src/main/scala/com/cjy/study1/wd/WordCount.scala
+++ sparkstudy01/src/main/scala/com/cjy/study1/wd/WordCount.scala
@@ -14,8 +14,17 @@ object WordCount {
 
 //            val conf = new SparkConf().setAppName("WC---") //打包提交到spark下运行时
             val sc = new SparkContext(conf)
-            sc.textFile(args(0)).flatMap(_.split(" ")).map((_, 1)).reduceByKey(_+_, 1).sortBy(_._2, false).saveAsTextFile(args(1))
-//            sc.textFile("in/").flatMap(_.split(" ")).map((_, 1)).reduceByKey(_+_, 1).sortBy(_._2, false).saveAsTextFile("out/")
+//            sc.textFile(args(0)).flatMap(_.split(" ")).map((_, 1)).reduceByKey(_+_, 1).sortBy(_._2, false).saveAsTextFile(args(1))
+            sc.textFile("in/").flatMap(_.split(" ")).map((_, 1)).reduceByKey(_+_, 1).sortBy(_._2, false).saveAsTextFile("out/")
+        /**
+          * 步骤解析：
+          * textFile：读取一行行数据
+          * flatMap：每行数据按照空格符号分割
+          * map：转换数据形态，hello-->(hello,1)
+          * reduceByKey：将相同key聚集在一起，并计算v的值，第二个参数表示使用几个分区，聚合效果：(hello,1),(hello,1)-->(hello,2)
+          * sortBy：排序，按照v大小排序，false 降序排序
+          * saveAsTextFile：保存到本地文件，当前工程下的out文件夹下面，切记此文件夹不能存在，否则运行报错
+          */
             sc.stop()
       }
 }
--- sparkstudy01/src/main/scala/com/cjy/study2/RDD/keyValue/reduceByKeyRDD3.scala
+++ sparkstudy01/src/main/scala/com/cjy/study2/RDD/keyValue/reduceByKeyRDD3.scala
@@ -3,18 +3,24 @@ package com.cjy.study2.RDD.keyValue
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
-/**
+/** reduceByKey(func: (V, V) => V, numPartitions: Int)
   * reduceByKey：在一个(K,V)的RDD上调用，返回一个(K,V)的RDD，使用指定的reduce函数，将相同key的值聚合到一起，
-  *               reduce任务的个数可以通过第二个可选的参数来设置。
+  * numPartitions：任务的个数可以通过第二个可选的参数来设置。
   */
 object reduceByKeyRDD3 {
 
   def main(args: Array[String]): Unit = {
-    val conf: SparkConf = new SparkConf().setAppName("t_demo").setMaster("local[*]")
+    val conf: SparkConf = new SparkConf().setAppName("t_demo").setMaster("local[1]")
     val sc: SparkContext = new SparkContext(conf)
-    val arrRDD: RDD[(String, Int)] = sc.parallelize(List(("female", 3), ("female", 1), ("male", 5), ("female", 5), ("male", 2), ("male",1)))
+    val arrRDD: RDD[(String, Int)] = sc.parallelize(List(("female", 3), ("female", 1), ("male", 5), ("female", 5), ("male", 2), ("male", 1)))
     //(x, y) => x + y，相同key的两个值进行相加
-    val reRDD: RDD[(String, Int)] = arrRDD.reduceByKey((x, y) => x + y)
+    val reRDD: RDD[(String, Int)] = arrRDD.reduceByKey((x, y) => x + y,1)
+//    val reRDD: RDD[(String, Int)] = arrRDD.reduceByKey(_+_,1)
     reRDD.foreach(println(_))
+
+    /**
+      * (male,8)
+      * (female,9)
+      */
   }
 }
